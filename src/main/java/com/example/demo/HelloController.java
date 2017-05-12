4148594f2cf68b8b29732494cc3e9697da45be97
330556994 <330556994@qq.com>
2017-05-12 14:15:03 +0800

    初始化提交

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloController {
+    @Value("${gongtao.name}")
+    private String name;
+
+    @RequestMapping("/")
+    //@ResponseBody
+    String home(Model model) {
+	model.addAttribute("name",name);
+        return "index";
+    }
+}
